3fc15cb2f5ec77455af2e8d4577c123a353a0e56
Elias Lopes <116045632+eliaszlsp@users.noreply.github.com>
2024-10-03 21:19:03 +0000

    g

---
+++ ex3/pergunta.js
@@ -0,0 +1,18 @@
+/* function primeira (segunda){
+    const nome = prompt ("Qual o seu nome")
+    segunda(nome)
+}
+primeira (function(nomerecebido){
+    alert(`o seu nome é ${nomerecebido}`)
+}) */
+/* const pessoa = function (nome) {
+    const getNome = function(){
+        return nome;
+    };
+    return getNome;
+
+}
+const aluno = pessoa("gabriel")
+const resultado = aluno ();
+console.log (pessoa, "aqui")
+ */
--- ex3/script.js
+++ ex3/script.js
@@ -1,14 +1,35 @@
-let formulario = document.getElementById("form-resultado")
-let resultado = document.getElementById("resultado")
+/* let formulario = document.getElementById("form-resultado")
+
 
 ;
 
- 
+
+
 formulario.addEventListener("submit", function(event)  {
     event.preventDefault()
     const numero1 = document.getElementById("numero1").value
     const numero2 = document.getElementById("numero2").value
-     let soma = Number(numero1) +  Number(numero2)        
-     resultado.innerHTML = soma    
+     let soma = Number(numero1) +  Number(numero2)
+     let subtrair = Number(numero1) - Number(numero2) 
+     let multiplicar = Number(numero1) * Number(numero2)
+     let dividir = Number(numero1) / Number(numero2)    
+     resultado.innerHTML = soma, subtrair, multiplicar, dividir    
+     tv = "fala"          
 })
+ */
+ /* function tv () { return "oi" }  */
 
+ 
+
+/*  
+console.log(tv, "oi") */
+
+let resultado = document.getElementById("resultado")
+const botaosomar = document.getElementById("somar")
+botaosomar.addEventListener("click", () =>{
+    const numero1 = document.getElementById("numero1").value
+    const numero2 = document.getElementById("numero2").value
+    let soma = Number(numero1) +  Number(numero2)
+    resultado.innerHTML = soma
+
+} )
